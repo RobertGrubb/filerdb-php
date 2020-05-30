24aea9a2a6723d8a3c57c6e01fc451cb34dbcfbb
Matt Grubb <mgrubb0507@gmail.com>
2020-05-30 10:47:31 -0400

    Starting unit tests

---
+++ tests/FilerDBTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace FilerDB\Tests;
+
+use FilerDB\Instance;
+use PHPUnit\Framework\TestCase;
+
+class FilerDBTest extends TestCase
+{
+  /**
+   * @var FilerDB\Instance
+   */
+  private static $filerdb;
+
+  // Path to the example database directory
+  private static $databaseDir = __DIR__ . '/../example/database';
+
+  public static function setUpBeforeClass() {
+
+    self::$filerdb = new Instance([
+
+      // Required
+      'path' => self::$databaseDir,
+    ]);
+  }
+
+  /**
+   * Make sure the database list comes back as
+   * an array.
+   */
+  public function testDatabaseList () {
+    $list = self::$filerdb->databases->list();
+    $this->assertEquals(true, is_array($list));
+  }
+}
